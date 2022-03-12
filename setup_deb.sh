26515be27f9a093d1d3e6e65a62be8d25bc84c94
Lars Hudson Lunde <laffen47@yahoo.no>
2022-03-12 17:32:27 +0100

    Update setup_deb.sh

--- setup_deb.sh
+++ setup_deb.sh
@@ -2,4 +2,4 @@
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs redis
-npm install express express-basic-auth redis body-parser path
+npm install express express-basic-auth redis body-parser path dotenv
