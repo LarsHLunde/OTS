bf65979ba5d751c1a40dd73b16ca3da978c31e0e
Lars Hudson Lunde <laffen47@yahoo.no>
2022-03-12 21:03:00 +0100

    dsa

--- frontend/js/script-secret.js
+++ frontend/js/script-secret.js
@@ -2,7 +2,7 @@ function init() {
   var out = document.getElementById("output");
   out.innerHTML = "Getting secret...";
 
-  $.get("/api/secret/" + window.location.href.split("/").slice(-1), function(data) {
+  $.get("APP_PREPEND_MARKER/api/secret/" + window.location.href.split("/").slice(-1), function(data) {
     var count = (data.match(/\n/g) || []).length;
     out.style.minHeight = toString(10+count*21) + "px"
     out.innerHTML = data.replaceAll("\n","<br />");
