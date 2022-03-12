50cf9c30f08ed1fc97c6ec7aeae41e930ad9cea3
Lars Hudson Lunde <laffen47@yahoo.no>
2022-03-12 17:32:09 +0100

    Update setup_yum.sh

--- setup_yum.sh
+++ setup_yum.sh
@@ -2,5 +2,5 @@
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs redis
-npm install express express-basic-auth redis body-parser path
+npm install express express-basic-auth redis body-parser path dotenv
 sudo systemctl enable redis -y
